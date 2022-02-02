712a8e3ed07b9b4e6d2eceb5c518b951c54079c7
Abigael Anyanwu <abigaelanyanwu@gmail.com>
2022-02-02 13:12:47 +0000

    first commit

---
+++ modules/Gallery.js
@@ -0,0 +1,30 @@
+const Gallery = () => `
+    <input type="file" name="photo" id="uploadPhoto">
+    <label for="uploadPhoto">
+        <i class="add-photo fa fa-plus-square"></i>
+    </label>
+
+    <div class="gallery-nav"></div>
+
+        <section class="gallery">
+            <a href="#" class="item">
+                <img src="https://images.pexels.com/photos/8747009/pexels-photo-8747009.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+            </a>
+            <a href="#" class="item">
+                <img src="https://images.pexels.com/photos/10530054/pexels-photo-10530054.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+            </a>
+            <a href="#" class="item">
+                <img src="https://images.pexels.com/photos/6759350/pexels-photo-6759350.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+            </a>
+            <a href="#" class="item">
+                <img src="https://images.pexels.com/photos/10145718/pexels-photo-10145718.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+            </a>
+            <a href="#" class="item">
+                <img src="https://images.pexels.com/photos/1605177/pexels-photo-1605177.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+            </a>
+            <a href="#" class="item">
+                <img src="https://images.pexels.com/photos/1416377/pexels-photo-1416377.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+            </a>
+        </section>
+        `
+        export default Gallery;
---
+++ modules/Nav.js
@@ -0,0 +1,15 @@
+const Nav = () => `
+    <nav>
+        <a href="#" class="logo">
+            <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="logo">
+        </a>
+        <input type="text" class="search" placeholder="Search" >
+        <span class="nav-links">
+            <a href=""><i class="fa fa-home"></i></a>
+            <a href="#"><i class="fa fa-location-arrow"></i></a>
+            <a href="#"><i class="fa fa-compass"></i></a>
+            <a href="#"><i class="fa fa-heart-o"></i></a>
+        </span>
+    </nav>
+`
+export default Nav;
