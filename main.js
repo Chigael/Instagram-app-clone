3c6dc8d9a6a51f36e6bab19cf8824e53654cc392
Abigael Anyanwu <abigaelanyanwu@gmail.com>
2022-02-02 18:57:47 +0000

    restructured commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import Bio from './modules/Bio.js';
 import Gallery from './modules/Gallery.js';
 import Nav from './modules/Nav.js';
 
+const rootDiv = document.getElementById('root')
 const Main = () => {
     return `
         ${Nav()}
@@ -12,4 +13,15 @@ const Main = () => {
     `
 }
 
-document.getElementById('root').innerHTML = Main();
+rootDiv.innerHTML = Main()
+
+    const editBio = document.querySelector('.edit-bio')
+
+    editBio.addEventListener('submit', () => {
+        event.preventDefault()
+
+    const nameInput = document.querySelector('#name').value
+    const nameOutput = document.querySelector('.name')
+
+    nameOutput.innerText = nameInput
+})
---
+++ modules/bio/Bio.js
@@ -0,0 +1,22 @@
+const Bio = () =>  {    
+
+    return `
+        <section class="bio">
+            <div class="profile-photo">
+                <img src="https://images.pexels.com/photos/1125328/pexels-photo-1125328.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile-photo">
+            </div>
+
+            <div class="profile-info">
+                <p class="name">Benzy La Creamy</p>
+                <button>Follow</button>
+            </div>
+
+            <form class="edit-bio">
+            <input type="text" id="name" placeholder="name" />
+            <button type="submit">Submit</button>
+            </form>
+        </section>
+`
+}
+
+export default Bio;
